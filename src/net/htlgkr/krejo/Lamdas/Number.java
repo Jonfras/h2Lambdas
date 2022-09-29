46de6eab98f1310cdef916c3ab376ecb53870751
Jonfras <jonas.kreuzh@gmail.com>
2022-09-29 21:59:23 +0200

    Number created

---
+++ src/net/htlgkr/krejo/Lamdas/Number.java
@@ -0,0 +1,14 @@
+package net.htlgkr.krejo.Lamdas;
+
+public class Number {
+    private double a;
+    private double b;
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+}
