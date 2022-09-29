4f85721e71d84c6ad976ab4dafe7492cdda1a525
Jonfras <jonas.kreuzh@gmail.com>
2022-09-29 21:57:02 +0200

    AbstractCalculator and CalculationOperation

---
+++ src/net/htlgkr/krejo/Lamdas/CalculationOperation.java
@@ -0,0 +1,6 @@
+package net.htlgkr.krejo.Lamdas;
+
+@FunctionalInterface
+public interface CalculationOperation {
+    Number calc(Number x, Number y);
+}
--- src/net/htlgkr/krejo/Lamdas/NumberTester.java
+++ src/net/htlgkr/krejo/Lamdas/NumberTester.java
@@ -55,9 +55,7 @@ public class NumberTester {
     }
 
 
-    public void setOddEvenTester(NumberTest oddTester) {
-        this.oddTester = oddTester;
-    }
+    public void setOddEvenTester(NumberTest oddTester) {this.oddTester = oddTester;}
     public void setPrimeTester(NumberTest primeTester) {
         this.primeTester = primeTester;
     }
