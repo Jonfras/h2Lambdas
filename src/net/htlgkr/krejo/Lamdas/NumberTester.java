e8c8056a7c3c8703651ead9f446d0498d49f5617
Jonfras <jonas.kreuzh@gmail.com>
2022-09-29 10:18:58 +0200

    NumberTester set Methods added

--- src/net/htlgkr/krejo/Lamdas/NumberTester.java
+++ src/net/htlgkr/krejo/Lamdas/NumberTester.java
@@ -1,12 +1,27 @@
 package net.htlgkr.krejo.Lamdas;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class NumberTester {
+    NumberTest oddTester;
+    NumberTest primeTester;
+    NumberTest palindromeTester;
 
     public NumberTester(String fileName){
 
     }
 
     public void setOddEvenTester(NumberTest oddTester){
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester){
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester){
 
     }
 
